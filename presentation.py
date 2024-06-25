806a414bd80a8cc7ce5edce171bdac6b383b8c8b
Matheus Foscarini Dias <matheus.foscarinid@gmail.com>
2024-06-25 13:21:35 -0300

    feat: adding more examples and docs to the examples

--- presentation.py
+++ presentation.py
@@ -1,26 +1,49 @@
 from adt_deque import ADTDeque
 from linked_deque import LinkedDeque
 
+def test_deque_methods():
+  deque: ADTDeque = LinkedDeque()
+
+  for i in range(10):
+    deque.insert_last(i)
+
+  print('Deque preenchido: ', deque)
+
+  deque.remove_first()
+  print('Removendo o primeiro elemento: ', deque)
+  
+  deque.insert_first(0)
+  print('Adicionando o primeiro elemento de volta: ', deque)
+
+  deque.remove_last()
+  print('Removendo o último elemento: ', deque)
+
+  first_item = deque.peek_first()
+  last_item = deque.peek_last()
+
+  print('Primeiro elemento do deque: ', first_item)
+  print('Último elemento do deque: ', last_item)
+  print('Tamanho da Deque: ', len(deque))
+  
+  print('Iterando sobre o deque: ', end='')
+  
+  for item in deque:
+    print(item, end=', ')
+
+def test_empty_deque():
+  deque: ADTDeque = LinkedDeque()
+  print('Deque vazio: ', deque)
+  
+  print('Tamanho da Deque vazia: ', len(deque))
+  print('Deque vazio? ', deque.is_empty())
+  
+  try:
+    print('Tentando remover o primeiro elemento da Deque vazia...')
+    deque.remove_first()
+  except Exception as e:
+    print('Erro:', e)
+  
+  
 if __name__ == '__main__':
-    list: ADTDeque = LinkedDeque()
-
-    for i in range(10):
-        list.insert_last(i)
-
-    print('Lista preenchida: ', list)
-
-    list.remove_first()
-    print('Removendo o primeiro elemento: ', list)
-    
-    list.insert_first(0)
-    print('Adicionando o primeiro elemento de volta: ', list)
-
-    list.remove_last()
-    print('Removendo o último elemento: ', list)
-
-    first_item = list.peek_first()
-    last_item = list.peek_last()
-
-    print('Primeiro elemento da lista: ', first_item)
-    print('Último elemento da lista: ', last_item)
-    print('Tamanho da lista: ', len(list))
+  test_deque_methods()
+  test_empty_deque()
